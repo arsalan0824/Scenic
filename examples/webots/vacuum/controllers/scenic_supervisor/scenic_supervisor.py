758eb635828180fe38088186e878e826d3141a12
Kay <ksv14ksv14@gmail.com>
2025-06-16 15:45:17 -0700

    cleaned up

--- examples/webots/vacuum/controllers/scenic_supervisor/scenic_supervisor.py
+++ examples/webots/vacuum/controllers/scenic_supervisor/scenic_supervisor.py
@@ -13,35 +13,40 @@ from stable_baselines3 import SAC,PPO
 
 
  
-print("Begining Supervisor Script")
-supervisor = Supervisor()
-print("Supervisor node collected")
-simulator = WebotsSimulator(supervisor)
+supervisor = Supervisor() # Collect the Supervisor node from the simulation
+simulator = WebotsSimulator(supervisor) # Create an instance of the WebotsSImulator with the corresponding node
 
 
 prefix = scenic.__file__[:-22]
 scenario = scenic.scenarioFromFile(prefix +  "examples/webots/vacuum/vacuum.scenic",
                                    model="scenic.simulators.webots.model",
-                                   mode2D=False)
+                                   mode2D=False) # generate the scenario from the corresponding Scenic file
 
 
 
-action_space = gym.spaces.Box(low=0.0, high=1,shape=(2,))
-observation_space = gym.spaces.Box(low=0, high=float('10000'),shape=(8,),dtype=np.float64)
-
+action_space = gym.spaces.Box(low=-1.0, high=1.0 ,shape=(2,))  # Defines the possible actions of the agent
+observation_space = gym.spaces.Box(low=0, high=float('10000'),shape=(8,),dtype=np.float64) # defines the range of observations of the agent
+max_steps = 5000
 env = ScenicGymEnv(scenario, 
                    simulator, 
                    render_mode=None, 
-                   max_steps=1000, 
+                   max_steps=max_steps, 
                    action_space=action_space,
-                   observation_space=observation_space) # max_step is max step for an episode
+                   observation_space=observation_space) # max_step is max step for an episode - Create an enviroment instance
+
 #TODO FIX THIS -> WARNING: DEF IROBOT_CREATE Create > HingeJoint CREATE_LEFT_WHEEL > RotationalMotor "left wheel motor": The requested velocity 260.145 exceeds 'maxVelocity' = 16.129.
 
-#check_env(env, skip_render_check=True)
+episodes=50
+total_timesteps = max_steps * episodes
+print(total_timesteps)
 
-model = PPO("MlpPolicy", env, verbose=2)
-model.learn(total_timesteps=1000000)
-model.save("vacuum_agent")
+model = PPO("MlpPolicy", env, verbose=2) # Create an instance of an agent 
+model.learn(total_timesteps=total_timesteps)          # train the agent over a set number of steps
+model.save("vacuum_agent")               # Save the model after training
+
+
+
+
 
 
 
