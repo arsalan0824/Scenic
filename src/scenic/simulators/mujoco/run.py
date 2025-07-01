128b91f700c6ba54a0b18b7a3e4704bb899e23bc
Kay <ksv14ksv14@gmail.com>
2025-07-01 10:42:38 -0700

    testing mujoco

--- src/scenic/simulators/mujoco/run.py
+++ src/scenic/simulators/mujoco/run.py
@@ -3,22 +3,28 @@ from scenic.gym import ScenicGymEnv
 from scenic.simulators.mujoco.simulator import MujocoSimulator
 from scenic.core.scenarios import Scene
 
+import numpy as np
+import gymnasium as gym
+
 if __name__ == "__main__":
-    SAMPLES = 1
 
-    for sample_index in range(SAMPLES):
-        simulator = MujocoSimulator(xml="")
-        scenario = scenic.scenarioFromFile("simple.scenic")
+    observation_space = gym.spaces.Box(-np.inf, np.inf, (23,), np.float64)
+    action_space      = gym.spaces.Box(-2.0, 2.0, (7,), np.float64)
 
 
-        env = ScenicGymEnv(scenario, simulator, render_mode=None)
-        env.reset()
-        episode_over = False
-        while not episode_over:
-            action = env.action_space.sample() # dummy here
+    simulator = MujocoSimulator(xml="",use_default_arena=False)
+    scenario = scenic.scenarioFromFile("simple.scenic")
 
-            observation, reward, terminated, truncated, info = env.step(action)
-            print(observation)
-            episode_over = terminated or truncated
+    env = ScenicGymEnv(scenario, simulator, render_mode="human", observation_space=observation_space, action_space=action_space,max_steps=100000)
+    env.reset()
+    episode_over = False
+    while not episode_over:
+        action = env.action_space.sample() # dummy here
 
-        env.close()
+       # print(action)
+
+        observation, reward, terminated, truncated, info = env.step(action)
+        #print(observation[:12])
+        episode_over = terminated or truncated
+
+    env.close()
