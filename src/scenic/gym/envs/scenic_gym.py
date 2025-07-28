a4e0f95793968d85d05d9d9e59acdb3a55761965
Ethanc143 <introverteddgfly@gmail.com>
2025-07-28 11:24:34 -0700

    last version with buffer saving

--- src/scenic/gym/envs/scenic_gym.py
+++ src/scenic/gym/envs/scenic_gym.py
@@ -1,5 +1,6 @@
 from scenic.core.simulators import Simulator, Simulation, TerminationType
 from scenic.core.scenarios import Scenario
+from scenic.core.errors import setDebuggingOptions
 import gymnasium as gym
 from gymnasium import spaces
 from typing import Callable
@@ -7,7 +8,19 @@ from typing import Callable
 import random
 import numpy as np
 
+import pandas as pd
+
+setDebuggingOptions(verbosity=2)
+
 #TODO make ResetException
+
+file_path = "../../../../../output.csv"
+
+def write_csv(name, coverage, collisions):
+    row = [name] + list(coverage) + list(collisions)
+    columns = ["name"] + [f"coverage_{i}" for i in range(len(coverage))] + [f"collisions_{i}" for i in range(len(collisions))]
+    df = pd.DataFrame([row], columns=columns)
+    df.to_csv(file_path, index=False, mode='a', header=columns)
 class ResetException(Exception):
     def __init__(self):
         super().__init__("Resetting")
@@ -44,12 +57,18 @@ class ScenicGymEnv(gym.Env):
         self.feedback_fn = feedback_fn
         
         #CHANGEABLE STUFF, CHECK BEFORE EACH RUN ALSO CHECK THAT THE BUFFER IS EMPTY
-        self.training_method = "LP"
-        self.use_plr = False
-        self.use_verifai = True
+        self.training_method = "EL"
+        self.use_plr = True
+        self.use_verifai = False
         #Random: default
         #LP: Prioritized level replay based off learning potential
         #GAE: Generalized advantage estimation NOTE I HAVEN"T ADDED VEIRFAI + GAE YET
+        #EL: episode length, probability distribution based off inverse of episode length, need termination
+        
+        #changeable stuff for saving data to csv
+        self.save_to_csv = True # whether to save data to csv
+        self.run_name = "default_run" # name of the run, used for saving data to csv
+        self.total_episodes = 10 # total number of episodes to run, used for saving data to csv
         self.is_continuation = False # are you reloading the buffer form the disk
         self.buffer_p = 0.5 # probability of resampling
         
@@ -64,12 +83,17 @@ class ScenicGymEnv(gym.Env):
         self.working_index = -1
         self.flag = 0
         self.counting_reward = 0
+        self.steps_taken = 0
         
-        if self.use_plr and self.training_method not in ("LP", "GAE"):
+        if self.use_plr and self.training_method not in ("EL", "LP", "GAE"):
             raise ValueError(
                 f"use_plr=True but training_method={self.training_method!r}. "
                 "Must be one of 'LP' or 'GAE' if use_plr is enabled."
             )
+        
+        #information recording
+        self.episode_coverages = []  
+        self.episode_collisions = []  
 
     def _make_run_loop(self):
         while True:
@@ -77,7 +101,7 @@ class ScenicGymEnv(gym.Env):
                 scene = None
                 self.is_resampling = random.uniform(0, 1) < self.buffer_p
                 #sample or resample scenes
-                if not self.use_plr and self.training_method != "GAE" and self.training_method != "LP":
+                if not self.use_plr:
                     scene, _ = self.scenario.generate(feedback=self.feedback_result)
                 elif self.training_method == "LP" and len(self.buffer_learning_potential) != len(self.buffer_last_reward):
                     # finish sampling doubled scene
@@ -106,10 +130,10 @@ class ScenicGymEnv(gym.Env):
                 
                 self.counting_reward = 0
                 with self.simulator.simulateStepped(scene, maxSteps=self.max_steps) as simulation:
-                    steps_taken = 0
+                    self.steps_taken = 0
                     # this first block before the while loop is for the first reset call
                     done = lambda: not (simulation.result is None) or (simulation.get_truncation()) # allows for early truncation
-                    truncated = lambda: (steps_taken >= self.max_steps)  # TODO handle cases where it is done right on maxsteps
+                    truncated = lambda: (self.steps_taken >= self.max_steps)  # TODO handle cases where it is done right on maxsteps
                     observation = simulation.get_obs()
                     info = simulation.get_info() 
                     actions = yield observation, info
@@ -118,7 +142,7 @@ class ScenicGymEnv(gym.Env):
                         # Probably good that we advance first before any action is set.
                         # this is consistent with how reset works
                         simulation.advance()
-                        steps_taken += 1
+                        self.steps_taken += 1
                         observation = simulation.get_obs()
                         info = simulation.get_info()
                         reward = simulation.get_reward()
@@ -142,6 +166,13 @@ class ScenicGymEnv(gym.Env):
                     print("Exitedwhile loop")
             except ResetException:
                 print("reset exception caught")
+                print(f"Episode coverages: {self.episode_coverages}")
+                print(f"Mean and std of coverages: {np.mean(self.episode_coverages)} and {np.std(self.episode_coverages)}")
+                print(f"Episode collisions: {self.episode_collisions}")
+                print(f"Mean and std of collisions: {np.mean(self.episode_collisions)} and {np.std(self.episode_collisions)}")
+                if len(self.episode_coverages) == self.total_episodes and self.save_to_csv:
+                    print("saved data to csv")
+                    write_csv(np.concatenate([np.array([f"{self.run_name}"]), self.episode_coverages, self.episode_collisions]))
                 continue
 
     def reset(self, seed=None, options=None): # TODO will setting seed here conflict with VerifAI's setting of seed?
@@ -161,7 +192,15 @@ class ScenicGymEnv(gym.Env):
         assert not (self.loop is None), "self.loop is None, have you called reset()?"
 
         observation, reward, terminated, truncated, info = self.loop.send(action)
+        
+        if terminated or truncated:
+            self.episode_coverages.append(info.get("coverage", 0))
+            self.episode_collisions.append(info.get("collisions", 0))
+        
         return observation, reward, terminated, truncated, info
+    
+    def get_coverage(self):
+        return sum(self.episode_coverages) / len(self.episode_coverages) if self.episode_coverages else 0 
 
     def render(self): # TODO figure out if this function has to be implemented here or if super() has default implementation
         """
@@ -175,23 +214,28 @@ class ScenicGymEnv(gym.Env):
         self.simulator.destroy()
         
     def logScores(self):
+        if self.training_method == "Random":
+            return
         total_reward = self.counting_reward
         if(total_reward == 0):
             print("TOTAL REWRAD is 0! suspiciosu!")
         #log rewards and learning potential
         if self.flag == 0:
             #double sampling
-            lp = abs(total_reward - self.buffer_last_reward[self.working_index]) + 1e-8
-            if self.use_verifai:
-                self.feedback_result = -lp
-            self.buffer_learning_potential = np.append(self.buffer_learning_potential, lp)
-            self.buffer_last_reward[self.working_index] = total_reward
-            print("finished double sampling")
+            if(self.working_index >= len(self.buffer_last_reward)):
+                print(f"Warning: working index {self.working_index} is out of bounds for buffer_last_reward with length {len(self.buffer_last_reward)}")
+            else:
+                lp = abs(total_reward - self.buffer_last_reward[self.working_index]) + 1e-8
+                if self.use_verifai:
+                    self.feedback_result = -lp
+                self.buffer_learning_potential = np.append(self.buffer_learning_potential, lp)
+                self.buffer_last_reward[self.working_index] = total_reward
+                print("finished double sampling")
         elif self.flag == 1:
             #resampling
             if(self.working_index >= len(self.buffer_last_reward)):
                 print(f"Warning: working index {self.working_index} is out of bounds for buffer_last_reward with length {len(self.buffer_last_reward)}")
-            if self.training_method == "LP":
+            elif self.training_method == "LP":
                 lp = abs(total_reward - self.buffer_last_reward[self.working_index]) + 1e-8
                 if self.use_verifai:
                     self.feedback_result = -lp
@@ -199,6 +243,11 @@ class ScenicGymEnv(gym.Env):
                 self.buffer_last_reward[self.working_index] = total_reward
             elif self.training_method == "GAE":
                 lp = total_reward
+                self.buffer_learning_potential[self.working_index] = lp
+            elif self.training_method == "EL":
+                lp = 1 / (self.steps_taken + 100)
+                if(self.steps_taken <= 50):
+                    lp = 0
                 self.buffer_learning_potential[self.working_index] = lp
             else:
                 print("BIG ISSUE, resmaled but not special PLR")
@@ -208,6 +257,12 @@ class ScenicGymEnv(gym.Env):
             elif self.training_method == "GAE":
                 lp = total_reward
                 self.buffer_learning_potential = np.append(self.buffer_learning_potential, lp)
+            elif self.training_method == "EL":
+                lp = 1 / (self.steps_taken + 100)
+                if(self.steps_taken <= 50):
+                    lp = 0
+                self.buffer_learning_potential = np.append(self.buffer_learning_potential, lp)
+                self.buffer_last_reward = np.append(self.buffer_last_reward, total_reward)
             else:
                 print("BIG ISSUE, resampled but not special PLR")
             print("appened last reward")
