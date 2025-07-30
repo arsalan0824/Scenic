62c7f53e460bbf5310a4ebaa586b7b78d3723c60
arsalan0824 <arsyisawsome@gmail.com>
2025-07-30 16:44:23 -0500

    7/30

--- examples/webots/vacuum/controllers/scenic_supervisor/scenic_supervisor.py
+++ examples/webots/vacuum/controllers/scenic_supervisor/scenic_supervisor.py
@@ -47,11 +47,10 @@ observation_space = gym.spaces.Dict({
     "rotation": gym.spaces.Box(low=np.array([-1,-1,-1,-1]), high=np.array([1,1,1,1]), shape=(4,), dtype=np.float64)
 })
 
-
 print("Action and observation spaces defined.")
 max_steps = 10000
 
-iterations = 10
+iterations = 50
 timesteps_per_itr = max_steps * 1
 
 scenario_template = scenic.scenarioFromFile(prefix + "examples/webots/vacuum/vacuum.scenic",
@@ -113,7 +112,7 @@ eval_env = Monitor(eval_env)
 final_model = PPO.load("PPO_vacuum_agent_latest")
 final_model.set_env(eval_env)
 
-mean_rwd, std_reward = evaluate_policy(final_model, eval_env, n_eval_episodes=10, render=False, deterministic=False)
+mean_rwd, std_reward = evaluate_policy(final_model, eval_env, n_eval_episodes=15, render=False, deterministic=False)
 
 print(f"After evaluation mean reward was : {mean_rwd:.2f} with std: {std_reward:.2f}")
 
