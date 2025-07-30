fab4dfb638b3db13c864897e8127c9c0d6a2f10a
Kay <ksv14ksv14@gmail.com>
2025-07-30 14:04:13 -0700

    recurrent model

--- examples/webots/vacuum/controllers/scenic_supervisor/scenic_supervisor.py
+++ examples/webots/vacuum/controllers/scenic_supervisor/scenic_supervisor.py
@@ -1,6 +1,5 @@
 from scenic.gym import ScenicGymEnv
 import scenic
-from scenic.simulators.newtonian_gym import NewtonianSimulator
 from scenic.simulators.webots import WebotsSimulator
 
 import gymnasium as gym
@@ -9,38 +8,38 @@ import numpy as np
 from controller import Supervisor
 
 from stable_baselines3.common.env_checker import check_env
-from stable_baselines3 import SAC,PPO
+from stable_baselines3 import A2C,PPO
 
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.evaluation import evaluate_policy
+from sb3_contrib import RecurrentPPO
 
 import matplotlib.pyplot as plt
 import time
 import gc
 
 start = time.time()
-
 supervisor = Supervisor() # Collect the Supervisor node from the simulation
-simulator = WebotsSimulator(supervisor) # Create an instance of the WebotsSImulator with the corresponding node
 prefix = scenic.__file__[:-22]
 
+simulator = WebotsSimulator(supervisor) # Create an instance of the WebotsSImulator with the corresponding node
 
-action_space = gym.spaces.Box(low=-1.0, high=1.0 ,shape=(2,))  # Defines the possible actions of the agent
+action_space = gym.spaces.MultiDiscrete([3,4])  # Defines the possible actions of the agent
 observation_space = gym.spaces.Dict({
-    "velocity": gym.spaces.Box(low=np.array([-1, -1]), high=np.array([1, 1]), shape=(2,),dtype=np.float64),
+    "velocity":  gym.spaces.Discrete(16),
     "sensor": gym.spaces.Box(low=np.array([0,0,0,0,0,0,0]), high=np.array([1,1,1,1,1,1,1]),shape=(7,),dtype=np.float64), # defines the range of observations of the agent
     "position": gym.spaces.Box(low=np.array([-1, -1]), high=np.array([1, 1]), shape=(2,),dtype=np.float64),
     "rotation": gym.spaces.Box(low=np.array([-1,-1,-1,-1]), high=np.array([1,1,1,1]), shape=(4,), dtype=np.float64)
 })
-
+                             
+max_steps = 10000
+episodes  = 100
+total_timesteps = max_steps * episodes
 
 scenario = scenic.scenarioFromFile(prefix +  "examples/webots/vacuum/vacuum.scenic",
                                 model="scenic.simulators.webots.model",
-                                mode2D=False,
-                                params={"is_couch":False})
-max_steps = 1000
-episodes  = 1
-total_timesteps = max_steps * episodes
+                                mode2D=False)
+             
 
 env = Monitor(ScenicGymEnv(scenario, 
                 simulator, 
@@ -50,35 +49,22 @@ env = Monitor(ScenicGymEnv(scenario,
                 observation_space=observation_space)) # max_step is max step for an episode - Create an enviroment instance
 
 
-model = PPO("MultiInputPolicy", env, verbose=2,seed=20,learning_rate=0.0002,ent_coef=0.05) # Create an instance of an agent 
+model = RecurrentPPO("MultiInputLstmPolicy", env,seed=20,ent_coef=0.05)
+#model = PPO.load("PPO_vacuum_agent_H50_R100_R50Full", env=env) # Create an instance of an agent 
+#model = PPO.load("PPO_vacuum_agent_entropy_test", env=env)
 model.learn(total_timesteps=total_timesteps)
-model.save("PPO_vacuum_agent")
+model.save("recurrent_PPO_test")
 
-#env.close() avoid closing the env as it will destroy the simulator instance aswell
+#env.close() #avoid closing the env as it will destroy the simulator instance aswell - won't accomplish what is needed here anyways
 # Cleanup to avoid assertion error when instantiating new scenario
-del model
-del env
-gc.collect()
+#del model
+#del env
+#gc.collect()
 
+mean_rwd, std = evaluate_policy(model, env=env, n_eval_episodes=10, deterministic=True)
 
-scenario = scenic.scenarioFromFile(prefix +  "examples/webots/vacuum/vacuum.scenic",
-                        model="scenic.simulators.webots.model",
-                        mode2D=False,
-                        params={"is_couch":True})
+print(f"Mean reward over 10 episodes :{mean_rwd} \n std was: {std}")
 
-             
-env = Monitor(ScenicGymEnv(scenario, 
-        simulator, 
-        render_mode=None, 
-        max_steps=max_steps, 
-        action_space=action_space,
-        observation_space=observation_space))
-
-
-model = PPO.load("PPO_vacuum_agent", env=env, verbose=2)
-model.learn(total_timesteps=total_timesteps)
-#model.save("PPO_vacuum_agent")
- 
 episodic_rewards = env.get_episode_rewards()
 fig,ax = plt.subplots()
 
@@ -87,13 +73,3 @@ ax.stem(range(len(episodic_rewards)), episodic_rewards)
 file_name = "../episode_rewards/PPO_policy_" + str(total_timesteps)  + ".png"
 plt.savefig(file_name,format='png')
 plt.show()
-    
-
-mean_rwd, std_reward = evaluate_policy(model, env, n_eval_episodes=3,render=False, deterministic=False)
-
-print(f"After evaluation mean reward was : {mean_rwd} with std: {std_reward}") # Save the model after training
-
-end = time.time()
-
-print(f" training time was {(end - start) / 60} minutes for {total_timesteps} timesteps")
-
